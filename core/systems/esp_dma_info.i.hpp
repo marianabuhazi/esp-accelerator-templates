2deae9f682f74b2e8fbacd9dbb692e9b8a62bc28
Marian Abuhazi <ma4107@columbia.edu>
2024-02-27 20:33:54 +0000

    :building_construction: C/C++ formatting for spandex-caches

--- core/systems/esp_dma_info.i.hpp
+++ core/systems/esp_dma_info.i.hpp
@@ -1,26 +1,22 @@
 // Copyright (c) 2011-2023 Columbia University, System Level Design Group
 // SPDX-License-Identifier: MIT
 
-inline dma_info_t& dma_info_t::operator=(const dma_info_t &other)
+inline dma_info_t &dma_info_t::operator=(const dma_info_t &other)
 {
-    index = other.index;
+    index  = other.index;
     length = other.length;
-    size = other.size;
+    size   = other.size;
     return *this;
 }
 
 inline bool dma_info_t::operator==(const dma_info_t &rhs) const
 {
-    return ((rhs.index == index)
-            && (rhs.length == length)
-            && (rhs.size == size));
+    return ((rhs.index == index) && (rhs.length == length) && (rhs.size == size));
 }
 
-inline ostream& operator<<(ostream& os, dma_info_t const &dma_info)
+inline ostream &operator<<(ostream &os, dma_info_t const &dma_info)
 {
-    os << "{" << dma_info.index  << ","
-              << dma_info.length  << ","
-              << dma_info.size << "}";
+    os << "{" << dma_info.index << "," << dma_info.length << "," << dma_info.size << "}";
     return os;
 }
 
@@ -34,4 +30,3 @@ inline void sc_trace(sc_trace_file *tf, const dma_info_t &v, const std::string &
     sstm_c << name << ".size";
     sc_trace(tf, v.size, sstm_c.str());
 }
-
